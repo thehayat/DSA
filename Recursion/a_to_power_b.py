150428b155b5a02b34db9c625966a56aeed39377
thehayat <hayatt143@gmail.com>
2019-08-11 17:51:22 +0530

    a to power b with recursion.

---
+++ Recursion/a_to_power_b.py
@@ -0,0 +1,16 @@
+
+def Recur_power(a, b):
+    if b == 0:
+        return 1
+    temp = Recur_power(a, int(b / 2))
+    if b % 2 == 0:
+        return temp*temp
+
+    else:
+        if b > 0:
+            return a * temp * temp
+        else:
+            return (temp*temp)/a
+
+if __name__ == "__main__":
+    print(Recur_power(2,-4))
