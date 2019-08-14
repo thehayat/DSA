143085653f6488411eba5d39d43c44b32d40b06d
thehayat <hayatt143@gmail.com>
2019-08-14 23:10:04 +0530

    Modified the Approch

--- Array/tripletFamily.py
+++ Array/tripletFamily.py
@@ -1,7 +1,7 @@
 "Question"
 # https://practice.geeksforgeeks.org/problems/triplet-family/1/?ref=self
 def findTriplet(arr,n):
-    arr.sort
+    arr.sort()
     arr_set = set(arr)
 
     # count = 0
@@ -11,10 +11,15 @@ def findTriplet(arr,n):
             if i != j:
                 if arr[i] + arr[j] in arr_set:
                     output.append((arr[i], arr[j], arr[i] + arr[j]))
-                if (arr[i] + arr[j]) > arr[-1]:
-                    break
+                
+                # if (arr[i] + arr[j]) > arr[-1]:
+                #     break
+                
+                if len(output) >=3:
+                    return output
                     # count += 1
     return output
+
 if __name__ == '__main__':
     testcase = int(input())
     for i in range(testcase):
